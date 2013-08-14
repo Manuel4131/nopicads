17d9a6d58c4f16ebbfff266f61b58df7ffad8453
Wei-Cheng Pan <legnaleurc@gmail.com>
2013-08-14 16:31:03 +0800

    Add grunt and npm configuration.

---
+++ Gruntfile.js
@@ -0,0 +1,20 @@
+module.exports = function (grunt) {
+  'use strict';
+
+  grunt.loadNpmTasks('grunt-mocha-test');
+
+  grunt.initConfig({
+    pkg: grunt.file.readJSON('package.json'),
+    mochaTest: {
+      test: {
+        src: ['test/**/*.js'],
+      },
+    },
+  });
+
+  grunt.registerTask('test', 'mochaTest');
+};
+
+// ex: ts=2 sts=2 sw=2 et
+// sublime: tab_size 2; translate_tabs_to_spaces true; detect_indentation false; use_tab_stops true;
+// kate: space-indent on; indent-width 2;
